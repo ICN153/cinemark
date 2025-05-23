1144dc30140b3072f81069f017d1866ff38b804f
Isaias Carlos <109770805+ICN153@users.noreply.github.com>
2025-05-23 19:00:42 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
+const toggleButton = document.querySelector('.menu__toogle__icom');
+  const menuItems = document.querySelectorAll('.nav__item.has-link');
+
+  toggleButton.addEventListener('click', () => {
+    menuItems.forEach(item => {
+      item.classList.toggle('show');
+    });
+  });
+
+
+ 
+// sugestões
+
+const estados = [
+  "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal",
+  "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul",
+  "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí",
+  "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia",
+  "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"
+];
+
+const input = document.getElementById("localizacao");
+const ul = document.getElementById("sugestoes");
+
+input.addEventListener("input", function () {
+  const termo = this.value.toLowerCase();
+  ul.innerHTML = "";
+
+  if (termo.length === 0) {
+    ul.style.display = "none";
+    return;
+  }
+
+  const filtrados = estados.filter(estado => estado.toLowerCase().includes(termo));
+
+  filtrados.forEach(estado => {
+    const li = document.createElement("li");
+    li.innerHTML = `<i class="fas fa-map-marker-alt"></i> ${estado}`;
+    ul.appendChild(li);
+  });
+
+  ul.style.display = filtrados.length ? "block" : "none";
+});
 
